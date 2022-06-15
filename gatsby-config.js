0ffd961e10e9e83e44e0906397155ea607f050cd
Jensen <okichek@gmail.com>
2022-06-15 12:31:14 +0800

    remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,58 +3,58 @@ module.exports = {
     title: `1000url`,
     siteUrl: `https://asdas:9000`,
   },
-  plugins: [
-    {
-      resolve: require.resolve(`../gatsby-plugin-advanced-sitemap`),
-      options: {
-        limitPerPage: -1,
-        query: `{
-            page2: allSitePage {
-              edges {
-                node {
-                  id
-                  path
-                }
-              }
-            }
-            product: allSitePage{
-              edges {
-                node {
-                  id
-                  path
-                }
-              }
-            }
-          }`,
-        mapping: {
-          page2: {
-            sitemap: `page2`,
-            serializer: (edges) => {
-              return edges.map(({ node }) => {
-                return {
-                  node: {
-                    id: node.id,
-                    slug: node.path,
-                  },
-                };
-              });
-            },
-          },
-          product: {
-            sitemap: `product`,
-            serializer: (edges) => {
-              return edges.map(({ node }) => {
-                return {
-                  node: {
-                    id: node.id,
-                    slug: node.path,
-                  },
-                };
-              });
-            },
-          },
-        },
-      },
-    },
-  ],
+  // plugins: [
+  //   {
+  //     resolve: require.resolve(`../gatsby-plugin-advanced-sitemap`),
+  //     options: {
+  //       limitPerPage: -1,
+  //       query: `{
+  //           page2: allSitePage {
+  //             edges {
+  //               node {
+  //                 id
+  //                 path
+  //               }
+  //             }
+  //           }
+  //           product: allSitePage{
+  //             edges {
+  //               node {
+  //                 id
+  //                 path
+  //               }
+  //             }
+  //           }
+  //         }`,
+  //       mapping: {
+  //         page2: {
+  //           sitemap: `page2`,
+  //           serializer: (edges) => {
+  //             return edges.map(({ node }) => {
+  //               return {
+  //                 node: {
+  //                   id: node.id,
+  //                   slug: node.path,
+  //                 },
+  //               };
+  //             });
+  //           },
+  //         },
+  //         product: {
+  //           sitemap: `product`,
+  //           serializer: (edges) => {
+  //             return edges.map(({ node }) => {
+  //               return {
+  //                 node: {
+  //                   id: node.id,
+  //                   slug: node.path,
+  //                 },
+  //               };
+  //             });
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // ],
 };
